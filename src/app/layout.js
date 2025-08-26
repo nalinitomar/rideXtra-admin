9aed3d8e0a4d08ab6e676acf0d4fbb47710f338a
AniketXenel <aniket.kumar@xenelsoft.co.in>
2025-08-26 15:10:04 +0530

    Logo change

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,14 @@
 import '../styles/globals.css';
 import { AuthProvider } from '@/context/AuthContext';
 
+
+export const metadata = {
+  title: "RideXtra Admin",
+  description: "Best app ever 🚀",
+  icons: {
+    icon: "/Nas-Logo.svg", // <-- place your custom favicon in /public/myicon.png
+  },
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- src/lib/apiConfig.js
+++ src/lib/apiConfig.js
@@ -1,3 +1,3 @@
 // lib/apiConfig.js
-// export const API_BASE_URL = 'http://localhost:8082/api/v1';
-export const API_BASE_URL = 'http://mobileappindia.com:3031/api/v1';
+export const API_BASE_URL = 'http://localhost:8082/api/v1';
+// export const API_BASE_URL = 'http://mobileappindia.com:3031/api/v1';
