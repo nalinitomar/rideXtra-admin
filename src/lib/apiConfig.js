8019346a5a4404a4a76e2279affd551d16f085ba
AniketXenel <aniket.kumar@xenelsoft.co.in>
2025-09-12 11:33:23 +0530

    ridextra vm

--- src/lib/apiConfig.js
+++ src/lib/apiConfig.js
@@ -1,5 +1,5 @@
 // lib/apiConfig.js
 // export const API_BASE_URL = 'http://localhost:8082/api/v1';
-export const API_BASE_URL = 'http://35.192.69.3:3030/api/v1';
+export const API_BASE_URL = 'http://34.131.52.186:3030/api/v1';
 // export const IMAGE_URL ="http://localhost:8082/uploads/"
-export const IMAGE_URL ="http://35.192.69.3:3030/uploads"
+export const IMAGE_URL ="http://34.131.52.186:3030/uploads"
