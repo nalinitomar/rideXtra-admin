2bb6a8c167848f0b0c32238a5b61ab93ca0e6217
AniketXenel <aniket.kumar@xenelsoft.co.in>
2025-11-10 15:29:30 +0530

    New

--- src/lib/apiConfig.js
+++ src/lib/apiConfig.js
@@ -1,5 +1,5 @@
 // lib/apiConfig.js
-export const API_BASE_URL = 'http://localhost:3030/api/v1';
-// export const API_BASE_URL = 'http://34.131.104.9:3030/api/v1';
+// export const API_BASE_URL = 'http://localhost:3030/api/v1';
+export const API_BASE_URL = 'http://34.131.104.9:3030/api/v1';
 // export const IMAGE_URL ="http://localhost:3030/uploads/"
 export const IMAGE_URL ="http://34.131.104.9:3030/uploads"
