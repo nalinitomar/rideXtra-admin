d35207fad31b9e686eb94367c2a326cee3704506
AniketXenel <aniket.kumar@xenelsoft.co.in>
2026-01-08 18:56:39 +0530

    New api

--- src/lib/apiConfig.js
+++ src/lib/apiConfig.js
@@ -1,5 +1,5 @@
 // lib/apiConfig.js
-export const API_BASE_URL = 'https://ridextra.ai/api/api/v1';
+export const API_BASE_URL = 'http://13.204.93.225:3000/api/v1';
 export const IMAGE_URL ="https://ridextra.ai/uploads"
 // export const API_BASE_URL = 'http://localhost:3030/api/v1';
 // export const IMAGE_URL ="http://localhost:3030/uploads"
